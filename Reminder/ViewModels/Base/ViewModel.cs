47335b24315a979a1fd8f653fb81498857acafe9
Alex4484 <levin.aleksandr.a@gmail.com>
2022-09-03 05:39:08 +0500

    Формиорвание инфраструктуры приложения

---
+++ Reminder/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reminder.ViewModels.Base
+{
+    public partial class ViewModel
+    {
+    }
+}
---
+++ Reminder/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reminder.ViewModels
+{
+    public partial class MainPageViewModel
+    {
+    }
+}
