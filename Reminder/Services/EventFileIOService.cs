2831d2498b36b300fb9dd3bfd9ecac901c216d9e
Alex4484 <levin.aleksandr.a@gmail.com>
2022-09-04 04:57:28 +0500

    промежуточный фикс

--- Reminder/Services/EventFileIOService.cs
+++ Reminder/Services/EventFileIOService.cs
@@ -6,65 +6,72 @@ using System.Linq;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
+using static Android.Provider.CalendarContract;
 
 namespace Reminder.Services
 {
     public class EventFileIOService
     {
-        private List<Event> EventsData = new();
+        //private List<Event> EventsData = new();
 
         public async Task<List<Event>> LoadEventsData()
         {
-            try
-            {
-                using var stream = await FileSystem.OpenAppPackageFileAsync("EventsData.json");
-                using var reader = new StreamReader(stream);
-                var events = await reader.ReadToEndAsync();
-                EventsData = JsonSerializer.Deserialize<List<Event>>(events);
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine(ex.Message);
-                await Shell.Current.DisplayAlert("Error", ex.Message, "Ok");
-                return GetTestDataAsync();
-            }
-            return EventsData;
+            //try
+            //{
+            //    using var stream = await FileSystem.OpenAppPackageFileAsync("EventsData.json");
+            //    using var reader = new StreamReader(stream);
+            //    var events = await reader.ReadToEndAsync();
+            //    EventsData = JsonSerializer.Deserialize<List<Event>>(events);
+            //}
+            //catch (Exception ex)
+            //{
+            //    Debug.WriteLine(ex.Message);
+            //    await Shell.Current.DisplayAlert("Error", ex.Message, "Ok");
+            //}
+            return default;
         }
 
         public async Task SaveEventsDataAsync(List<Event> events)
         {
+            //try
+            //{
+            //    using var stream = await FileSystem.OpenAppPackageFileAsync("EventsData.json");
+            //    using var reader = new StreamReader(stream);
+            //    var content = await reader.ReadToEndAsync();
+
+            //    var targetFile = Path.Combine(FileSystem.Current.AppDataDirectory, "EventsData.json");
+
+            //    content = JsonSerializer.Serialize<List<Event>>(events);
+
+            //    using var writeStream = File.OpenWrite(targetFile);
+            //    using var streamWriter = new StreamWriter(writeStream);
+
+            //    await streamWriter.WriteAsync(content);
+            //}
+            //catch (Exception ex)
+            //{
+            //    Debug.WriteLine(ex.Message);
+            //    await Shell.Current.DisplayAlert("Error", ex.Message, "Ok");
+            //}
+            
+        }
+
+        public async Task<List<Event>> GetTestDataAsync()
+        {
             try
             {
-                var path = FileSystem.Current.AppDataDirectory;
-                var targetFile = Path.Combine(path, "EventsData.json");
-
-                using var writeStream = File.OpenWrite(targetFile);
-                using var streamWriter = new StreamWriter(writeStream);
-
-                var content = JsonSerializer.Serialize(events);
-
-                await streamWriter.WriteAsync(content);
+                using var stream = await FileSystem.OpenAppPackageFileAsync("EventsTestData.json");
+                using var reader = new StreamReader(stream);
+                var eventsInString = await reader.ReadToEndAsync();
+                var events = JsonSerializer.Deserialize<List<Event>>(eventsInString);
+                return events;
             }
             catch (Exception ex)
             {
                 Debug.WriteLine(ex.Message);
                 await Shell.Current.DisplayAlert("Error", ex.Message, "Ok");
+                return new List<Event>();
             }
-            
-        }
-
-        private List<Event> GetTestDataAsync()
-        {
-            EventsData.AddRange(new Event[] 
-            {
-                new Event{Name = "First event", DateTimeEvent= DateTime.Now, Description="Description first event"},
-                new Event{Name = "Second event", DateTimeEvent= DateTime.Now, Description="Description second event"},
-                new Event{Name = "Third event", DateTimeEvent= DateTime.Now, Description="Description third event"},
-                new Event{Name = "Fourth event", DateTimeEvent= DateTime.Now, Description="Description fourth event"},
-                new Event{Name = "Fifth event", DateTimeEvent= DateTime.Now, Description="Description Fifth event"},
-                new Event{Name = "Sixth event", DateTimeEvent= DateTime.Now, Description="Description sixth event"}
-            });
-            return EventsData;
         }
     }
 }
