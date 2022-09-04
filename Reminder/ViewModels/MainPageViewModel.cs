d225895b09924503e35b56725868f33f5a7a4b37
Alex4484 <levin.aleksandr.a@gmail.com>
2022-09-04 21:29:27 +0500

    Привязка представления CreateEdit к модели представления

--- Reminder/ViewModels/MainPageViewModel.cs
+++ Reminder/ViewModels/MainPageViewModel.cs
@@ -24,7 +24,7 @@ namespace Reminder.ViewModels
             GetDataEvents();
         }
 
-        public async void GetDataEvents()
+        private async void GetDataEvents()
         {
             var events = await eventFileIOService.LoadEventsDataAsync();
 
